5562cf14fac85cf0f48a8c10d7aeb858cdd17a56
Vinit <vinit_sawant2000@yahoo.com>
2025-11-07 20:05:42 +0000

    Initialize EatClub Spring Boot app with Java25 and Maven.

---
+++ src/test/java/com/eatclub/EatClub/EatClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatclub.EatClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
